aa3f2e30ba1fb8d27d3d2bbee4bfcb35c599fc68
danjuma1 <hda0202@gmail.com>
2021-01-30 03:14:39 +0100

    build 2 views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+# view for index page 
+def index(request):
+    return render(request, '')
+
+# topics view
+def topics(request):
+    # show all topics arraged by date added
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    context = {'topics': topics}
+    return render(request, '', context)
+
+
